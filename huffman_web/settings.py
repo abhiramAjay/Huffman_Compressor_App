64830154b08171621e9ca0891d68021b256c5a36
Abhiram <abhiram.a@noctil.com>
2025-07-01 10:33:06 +0530

    re pushing

--- huffman_web/settings.py
+++ huffman_web/settings.py
@@ -17,7 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'compressor',  # ← your custom app
+    'backend.compressor',  # ← your custom app
 ]
 
 MIDDLEWARE = [
